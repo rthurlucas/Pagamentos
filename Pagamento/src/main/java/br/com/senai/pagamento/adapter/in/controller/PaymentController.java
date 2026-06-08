33758a99c54ba9b09fb4168d0401dc2993193fe4
rthurlucas <arthurlucasx696@gmail.com>
2026-06-08 19:37:34 -0300

    Implement PaymentController and PaymentService for payment processing

--- Pagamento/src/main/java/br/com/senai/pagamento/adapter/in/controller/PaymentController.java
+++ Pagamento/src/main/java/br/com/senai/pagamento/adapter/in/controller/PaymentController.java
@@ -4,18 +4,30 @@ import br.com.senai.pagamento.adapter.in.controller.response.PaymentDetailsRespo
 import br.com.senai.pagamento.adapter.in.controller.resquest.CreatePaymentRequest;
 import br.com.senai.pagamento.adapter.in.controller.resquest.RefundPaymentRequest;
 import br.com.senai.pagamento.application.port.in.StdPayament;
+import br.com.senai.pagamento.application.service.PaymentService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/pagameto/payment")
 public class PaymentController implements StdPayament {
+    private final PaymentService service;
+
+    public PaymentController(PaymentService service){
+        this.service = service;
+    }
     @Override
+    @PostMapping("/toPay")
     public ResponseEntity<PaymentDetailsResponse> toPay(CreatePaymentRequest data) {
-        return null;
+        return ResponseEntity.ok(service.cretePayment(data);
     }
 
     @Override
+    @PostMapping("/refund")
     public ResponseEntity<Void> toRefund(RefundPaymentRequest data) {
-        return null;
+        service.refundPayment(data);
+        return ResponseEntity.ok().build();
     }
 }
---
+++ Pagamento/src/main/java/br/com/senai/pagamento/application/service/PaymentService.java
@@ -0,0 +1,7 @@
+package br.com.senai.pagamento.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+}
